e14b62b8c2d8c3b4b97e4e11b34f097e81856e35
andresb39 <andresb39@gmail.com>
2023-01-31 15:57:15 -0500

    Validate TF manifest

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_ssoadmin_permission_set" "sso_permission_set" {
-  name         = var.group_name
-  instance_arn = tolist(data.aws_ssoadmin_instances.sso.arns)[0]
-  description  = var.description
+  name             = var.group_name
+  instance_arn     = tolist(data.aws_ssoadmin_instances.sso.arns)[0]
+  description      = var.description
   session_duration = var.session_duration
-  tags = var.tags
+  tags             = var.tags
 }
 resource "aws_ssoadmin_managed_policy_attachment" "sso_policy_attachment" {
   count              = length(var.policy_attachment)

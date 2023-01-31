d28a8a1c010b5fb4ecb39b446a4e1fffb3be4b39
andresb39 <andresb39@gmail.com>
2023-01-31 17:12:39 -0500

    Add target_id on workflow

--- example/complete/main.tf
+++ example/complete/main.tf
@@ -1,12 +1,12 @@
-module sso_permissions_set {
-  source = "../.."
-  group_name = "JB_Example"
-  description = "Permissions for DevOps Team"
-  inline_policy = data.aws_iam_policy_document.permissions.json
-  policy_attachment = [ "arn:aws:iam::aws:policy/AmazonEC2FullAccess", "arn:aws:iam::aws:policy/AmazonRDSFullAccess" ]
-  display_name = "MyGroupGroso..."
+module "sso_permissions_set" {
+  source               = "../.."
+  group_name           = "JB_Example"
+  description          = "Permissions for DevOps Team"
+  inline_policy        = data.aws_iam_policy_document.permissions.json
+  policy_attachment    = ["arn:aws:iam::aws:policy/AmazonEC2FullAccess", "arn:aws:iam::aws:policy/AmazonRDSFullAccess"]
+  display_name         = "MyGroupGroso..."
   description_identity = "Dream team"
-  target_id = "473530283000"
+  target_id            = "473530283000"
 
 
   tags = {
